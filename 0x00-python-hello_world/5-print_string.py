a0626f538302a58405c98ace356ead50d070f243
uriel46 <sampsonuriel@gmail.com>
2023-01-02 15:48:57 -0800

    hello world

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+string = "Holberton School"
+print(string*3)
+print(string[:9])
---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+string = "Python is an interpreted, interactive, ogject-oriented\
+programming language that combines remarkable power with very clear syntax"
+string = string[39:67] + string[107:112] + string[:6]
+print(string)
